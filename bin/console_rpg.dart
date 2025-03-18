5a56aaef656d001267d7c9f3d76b381b64a54dfd
arcmee <kihyun3297@gmail.com>
2025-03-18 16:44:48 +0900

    first commit

---
+++ bin/console_rpg.dart
@@ -0,0 +1,5 @@
+import 'package:console_rpg/console_rpg.dart' as console_rpg;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_rpg.calculate()}!');
+}
---
+++ test/console_rpg_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_rpg/console_rpg.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
